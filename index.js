7a7e46d86c930b979ad944b9ac6aeaf29d68cc99
Brodie Ashcraft <brodiea19@gmail.com>
2023-03-29 22:15:04 -0700

    Complete Array Filtering lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+const findMatching = (array, str) =>{
+    return array.filter(function (name){
+        return name.toLowerCase() === str.toLowerCase()
+    })
+}
+
+const fuzzyMatch = (array, str) =>{
+    return array.filter(function (name){
+        let splitName = name.split('');
+        let splitStr = str.split('');
+        return (splitName[0] === splitStr[0])
+    })
+}
+
+const matchName = (driver, str) =>{
+    return driver.filter(function(person){
+        return person.name === str
+    })
+}
